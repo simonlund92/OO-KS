ac17e12ea3f78451954341b47efd3cf0966a8cc6
Simon Lund <theburning_hill@hotmail.com>
2015-06-08 11:50:33 +0200

    Green bar. Oprettet stringbuilders og get metoder for V og I, hvor jeg tjekker på om der er for mange V'ere og I'ere. Hvis der er, appender jeg et X og sletter V'erne og for I'erne appender jeg et V og sletter I'erne. Derefter samgler jeg romertalene i min sb stringbuilder ved at appende dem sammen.

--- OO-KS/src/rt/Romertal.java
+++ OO-KS/src/rt/Romertal.java
@@ -1,16 +1,49 @@
 package rt;
 
 public class Romertal {
-  private String a,b,c;
- 
-  public Romertal(String a, String b){
+  private String a, b, c;
+  private StringBuilder sb = new StringBuilder( "" );
+  private StringBuilder sbI = new StringBuilder( "" );
+  private StringBuilder sbV = new StringBuilder( "" );
+  private StringBuilder sbX = new StringBuilder( "" );
+
+  public Romertal( String a, String b ) {
     this.a = a;
     this.b = b;
     c = a + b;
   }
 
   public String getFaceit() {
-    // TODO Auto-generated method stub
-    return "V";
+    getI();
+    getV();
+
+    return sb.append( sbX ).append( sbV ).append( sbI ).toString();
   }
+
+  private void getV() {
+    for ( int i = 0; i < c.length(); i++ ) {
+      if ( c.charAt( i ) == 'V' )
+        sbV.append( "V" );
+      if ( sbV.indexOf( "VV" ) == 0 ) {
+        sbV.delete( 0, 2 );
+        sbX.append( "X" );
+      }
+
+    }
+
+  }
+
+  private void getI() {
+    for ( int i = 0; i < c.length(); i++ ) {
+      if ( c.charAt( i ) == 'I' )
+        sbI.append( "I" );
+      if ( sbI.indexOf( "IIIII" ) == 0 ) {
+        sbI.delete( 0, 5 );
+        sbV.append( "V" );
+      }
+
+    }
+
+  }
+
 }

6bb3933fc36e729ff60c398728dd520196ddf9c7
Simon Lund <theburning_hill@hotmail.com>
2015-06-08 11:15:42 +0200

    Lavet en getmetode brugt fake it.

---
+++ OO-KS/src/rt/Romertal.java
@@ -0,0 +1,16 @@
+package rt;
+
+public class Romertal {
+  private String a,b,c;
+ 
+  public Romertal(String a, String b){
+    this.a = a;
+    this.b = b;
+    c = a + b;
+  }
+
+  public String getFaceit() {
+    // TODO Auto-generated method stub
+    return "V";
+  }
+}
---
+++ OO-KS/test/rt/TestRomertal.java
@@ -0,0 +1,18 @@
+package rt;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestRomertal {
+  
+    @Test
+    public void ikkeMereEndFireI(){
+      Romertal romertal = new Romertal("II", "III");
+    
+      assertEquals("V", romertal.getFaceit());
+    }
+}
